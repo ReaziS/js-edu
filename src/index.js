dc36fa1d0ffa1bae6d7f66ada82e0efa29a19197
ReaziS <kost9n98@yandex.ru>
2019-09-24 09:09:29 +0300

    feat: add tests has passed

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+module.exports = function getTimeForEducation(focus, knowProg, config){
+    let masterProg = 800;
+    let mediumProg = 1300;
+    return knowProg == true ? Math.ceil(masterProg / config[focus]):Math.ceil(mediumProg / config[focus]);
+        
+}
